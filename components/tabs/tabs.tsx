cf9c643732a17c6c6b8e7dfbf1ef0894c289e007
Daniel Hitzel <der-daniel@outlook.com>
2023-01-05 17:22:52 +0100

    wip

--- components/tabs/tabs.tsx
+++ components/tabs/tabs.tsx
@@ -14,7 +14,7 @@ const Tabs: FunctionComponent<TabsProps> = ({ tabs }) => {
   return (
     <div className="w-full">
       <div
-        className="flex items-center lg:justify-center gap-x-2 lg:gap-x-4 overflow-y-scroll py-6"
+        className="flex items-center lg:justify-center gap-x-2 lg:gap-x-4 overflow-y-auto py-6"
         data-aos="fade-up"
       >
         {tabs.map((tab, index) => (
